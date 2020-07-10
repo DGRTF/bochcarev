74eea270c741d2b61b7161d1baf47decf540195b
Your Name <futunplay@gmail.com>
2020-07-10 12:54:37 +0300

    Updates

---
+++ precache-manifest.72ebd4e6b5a56a1da2394333b967aed1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64d1109067068a5e745b26a530c84da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "756a580ce2bb7f99409e",
+    "url": "/static/css/main.fb517e8a.chunk.css"
+  },
+  {
+    "revision": "c60ee55d09ea73537770",
+    "url": "/static/js/2.9acecc85.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9acecc85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756a580ce2bb7f99409e",
+    "url": "/static/js/main.ed31a26e.chunk.js"
+  },
+  {
+    "revision": "b69956c6c0867e207e5b",
+    "url": "/static/js/runtime-main.7fa0c0c9.js"
+  },
+  {
+    "revision": "42185235733a09abcf42a0b115a1541b",
+    "url": "/static/media/carousel_item_1.42185235.jpg"
+  }
+]);
