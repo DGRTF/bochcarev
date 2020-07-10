0fd162e269791ac8c91bcf78bd7e973417f35250
Your Name <futunplay@gmail.com>
2020-07-10 15:07:40 +0300

    Updates

---
+++ precache-manifest.daf50d4c76717b458d39e971bf4851de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f10adeebf16e1e60e268a2c128fc4a",
+    "url": "/bochcarev/index.html"
+  },
+  {
+    "revision": "1127a81170c8011d529d",
+    "url": "/bochcarev/static/css/main.248b22f4.chunk.css"
+  },
+  {
+    "revision": "c60ee55d09ea73537770",
+    "url": "/bochcarev/static/js/2.9acecc85.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bochcarev/static/js/2.9acecc85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1127a81170c8011d529d",
+    "url": "/bochcarev/static/js/main.b3805895.chunk.js"
+  },
+  {
+    "revision": "7554bb934ae86ae2959d",
+    "url": "/bochcarev/static/js/runtime-main.81079386.js"
+  },
+  {
+    "revision": "42185235733a09abcf42a0b115a1541b",
+    "url": "/bochcarev/static/media/carousel_item_1.42185235.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72ebd4e6b5a56a1da2394333b967aed1.js"
+  "/bochcarev/precache-manifest.daf50d4c76717b458d39e971bf4851de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bochcarev/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
