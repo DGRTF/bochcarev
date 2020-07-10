f21204d585e555d38786c605e1b8bd60e11b843e
Your Name <futunplay@gmail.com>
2020-07-10 15:29:56 +0300

    Updates

--- precache-manifest.1a2e5d278b81fcb23e8b33a606030857.js
+++ precache-manifest.1a2e5d278b81fcb23e8b33a606030857.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f94917fb830f0dc9730b74896fd9d872",
+    "revision": "daedb3f9002b6ccaf36b49da4b9a5710",
     "url": "/bochcarev/index.html"
   },
   {
-    "revision": "e3121194c645fdcb2c47",
+    "revision": "eac908077d59b7230f5d",
     "url": "/bochcarev/static/css/main.248b22f4.chunk.css"
   },
   {
-    "revision": "3c1f8ca9d455075236a8",
-    "url": "/bochcarev/static/js/2.bf29d6b2.chunk.js"
+    "revision": "c60ee55d09ea73537770",
+    "url": "/bochcarev/static/js/2.9acecc85.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/bochcarev/static/js/2.bf29d6b2.chunk.js.LICENSE.txt"
+    "url": "/bochcarev/static/js/2.9acecc85.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e3121194c645fdcb2c47",
-    "url": "/bochcarev/static/js/main.fda7ca01.chunk.js"
+    "revision": "eac908077d59b7230f5d",
+    "url": "/bochcarev/static/js/main.8d0718df.chunk.js"
   },
   {
     "revision": "7554bb934ae86ae2959d",
